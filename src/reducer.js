aba8159d9d8a0c8290891471520b2a3e09b0efef
androidmini9x <androidmini92@gmail.com>
2022-02-07 17:41:09 +0300

    Applied Dispatch

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,23 @@ function changeState(state, action) {
   }
 }
 
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+function render() {
+  document.body.textContent = state.count;
+} 
+
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+// changeState(state, action);
+
+dispatch({ type: "counter/increment" });
+// => {count: 1}
+dispatch({ type: "counter/increment" });
+// => {count: 2}
+dispatch({ type: "counter/increment" });
+// => {count: 3} 
